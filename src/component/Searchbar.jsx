bf25358da5f363a5ff45d7d302bd29dd124a7e5a
Karema <karemahamdy51@gmail.com>
2024-11-11 03:37:52 +0200

    add fetch data and search feild

--- src/component/Searchbar.jsx
+++ src/component/Searchbar.jsx
@@ -1,39 +1,63 @@
+import { exerciseOptions, fetchData } from "../hooks/FetchData";
+import {  useEffect, useState } from "react";
 
 function SearchForm() {
+
+  const [search, setSearch] = useState('');
+  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+     console.log(bodyPartsData)
+      setBodyParts(['all', ...bodyPartsData]);
+
+    };
+
+    fetchExercisesData();
+  }, []);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      console.log(exercisesData)
+
+      const searchedExercises = exercisesData.filter(
+        (item) => item.name.toLowerCase().includes(search)
+          || item.target.toLowerCase().includes(search)
+          || item.equipment.toLowerCase().includes(search)
+          || item.bodyPart.toLowerCase().includes(search),
+      );
+
+  
+
+      setSearch('');
+      setExercises(searchedExercises);
+    }
+  };
+
   return (
-    <form className="flex items-center max-w-lg mx-auto">
-      <label htmlFor="voice-search" className="sr-only">
-        Search
-      </label>
-      <div className="relative w-full">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-    
-        </div>
-        {/* <InputDemo/> */}
-      
-      </div>
-      <button
-        type="submit"
-        className="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        <svg
-          className="w-4 h-4 me-2"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 20 20"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+  <>
+      <div className="flex gap-2 mb-3">
+        <input
+          className="w-[350px] lg:w-[1170px] h-[56px] font-bold  border-none focus:border-none rounded-md bg-white text-black px-4 py-2"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
           />
-        </svg>
+    
+    
+      <button
+        className="bg-[#FF2625] text-white font-normal h-[56px] w-[80px] lg:w-[173px] text-[14px] lg:text-[20px] rounded-md"
+        onClick={handleSearch}
+      >
         Search
       </button>
-    </form>
+      </div>
+          </>
   );
 }
 
---
+++ src/hooks/FetchData.jsx
@@ -0,0 +1,17 @@
+export const exerciseOptions = {
+  // const url = 'https://exercisedb.p.rapidapi.com/status';
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': 'a603b9f7edmsh45414b984ba4698p17919cjsn4f3fd0e587d5',
+      'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+  }
+}
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
+
+
