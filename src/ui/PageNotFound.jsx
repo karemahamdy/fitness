573f9e19a9e87bfd32124805535316022f22ba0b
Karema <karemahamdy51@gmail.com>
2024-11-14 03:36:41 +0200

    feat: aedd page not found

--- src/ui/PageNotFound.jsx
+++ src/ui/PageNotFound.jsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom";
 
 export default function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <section>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold text-white md:text-4x">Something's missing.</p>
+        
+        <Link to="/">
+  <button className="inline-flex text-white bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
+    Back to Homepage
+  </button>
+</Link>
+        </div>
+      </div>
+    </section>
   )
 }
